3c1b0f36f5304af0819cb7d145120234a3644e7b
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-11-08 15:25:38 +1100

    partial commit for troubleshooting SQL

--- db/js/scripts.js
+++ db/js/scripts.js
@@ -3,23 +3,54 @@
 //------------------------------------------------
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const cTable = require("console.table");
 
 //------------------------------------------------
 // Setting internal dependencies
 //------------------------------------------------
-const connection = require("./lib/connection");
+const connection = require("../../lib/connection");
+const showMenu = require("../../server");
 
 //------------------------------------------------
 // Functions
 //------------------------------------------------
 
-async function viewAllEmployees() {}
+async function viewEmployees() {
+  const query =
+    "SELECT * FROM employees JOIN roles ON employees.role_id=roles.role_id JOIN departments ON roles.department_id=departments.department_id ORDER BY employee_id";
+  connection.query(query, function (err, res) {
+    if (err) {
+      console.log(err);
+    }
+    let employeeArray = [];
+    for (let i = 0; i < res.length; i++) {
+      let employee = {
+        ID: res[i].employee_id,
+        Name: res[i].first_name + " " + res[i].last_name,
+        Title: res[i].title,
+        Department: res[i].department,
+        Salary: res[i].salary,
+        Manager: res[i].manager_id
+          ? res[res[i].manager_id - 1].first_name +
+            " " +
+            res[res[i].manager_id - 1].last_name
+          : null,
+      };
+      employeeArray.push(employee);
+    }
+    console.log("\n");
+    console.table(employeeArray);
+    showMenu();
+  });
+}
 
-async function viewAllDepartments() {}
+async function viewDepartments() {}
 
-async function viewAllRoles() {}
+async function viewRoles() {}
 
 async function viewEmployeesByManager() {}
+
+async function viewBudgetByDepartment() {}
 
 async function addEmployee() {}
 
@@ -27,10 +58,8 @@ async function addRole() {}
 
 async function addDepartment() {}
 
-async function deleteEmployee() {}
-
 async function updateEmployeeRole() {}
 
 async function updateEmployeeManager() {}
 
-async function viewBudgetByDepartment() {}
+async function deleteEmployee() {}
--- server.js
+++ server.js
@@ -40,34 +40,37 @@ async function showMenu() {
         name: "task",
         message: "What would you like to do?",
         choices: [
-          "View All Employees",
-          "View All Departments",
-          "View All Roles",
+          "View Employees",
+          "View Departments",
+          "View Roles",
           "View Employees By Manager",
+          "View Department Budget",
           "Add Employee",
           "Add Role",
           "Add Department",
-          "Delete Employee",
           "Update Employee's Role",
           "Update Employee's Manager",
-          "View Department Budget",
+          "Delete Employee",
           "Exit",
         ],
       },
     ])
     .then(function (answer) {
       switch (answer.task) {
-        case "View All Employees":
-          viewAllEmployees();
+        case "View Employees":
+          viewEmployees();
           break;
-        case "View All Departments":
-          viewAllDepartments();
+        case "View Departments":
+          viewDepartments();
           break;
-        case "View All Roles":
-          viewAllRoles();
+        case "View Roles":
+          viewRoles();
           break;
         case "View Employees By Manager":
           viewEmployeesByManager();
+          break;
+        case "View Department Budget":
+          viewBudgetByDepartment();
           break;
         case "Add Employee":
           addEmployee();
@@ -78,17 +81,14 @@ async function showMenu() {
         case "Add Department":
           addDepartment();
           break;
-        case "Delete Employee":
-          deleteEmployee();
-          break;
         case "Update Employee's Role":
           updateEmployeeRole();
           break;
         case "Update Employee's Manager":
           updateEmployeeManager();
           break;
-        case "View Department Budget":
-          viewBudgetByDepartment();
+        case "Delete Employee":
+          deleteEmployee();
           break;
         case "Exit":
           connection.end();
@@ -96,3 +96,5 @@ async function showMenu() {
       }
     });
 }
+
+module.exports = showMenu;
