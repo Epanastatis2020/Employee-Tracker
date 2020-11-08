1cc7521f05e8e07799d064dda216dad78c866285
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-11-08 16:32:19 +1100

    removed server.js, folded contents into scripts.js and amended package.json to reflect the changes. Fixed the connection errors.

--- db/js/scripts.js
+++ db/js/scripts.js
@@ -9,11 +9,85 @@ const cTable = require("console.table");
 // Setting internal dependencies
 //------------------------------------------------
 const connection = require("../../lib/connection");
-const server = require("../../server");
 
 //------------------------------------------------
-// Functions
+// Initialiser
 //------------------------------------------------
+
+init();
+
+async function init() {
+  showMenu();
+}
+
+//------------------------------------------------
+// Show Menu function
+//------------------------------------------------
+
+async function showMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+          "View Employees",
+          "View Departments",
+          "View Roles",
+          "View Employees By Manager",
+          "View Department Budget",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Update Employee's Role",
+          "Update Employee's Manager",
+          "Delete Employee",
+          "Exit",
+        ],
+      },
+    ])
+    .then(function (answer) {
+      switch (answer.task) {
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employees By Manager":
+          viewEmployeesByManager();
+          break;
+        case "View Department Budget":
+          viewBudgetByDepartment();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Update Employee's Role":
+          updateEmployeeRole();
+          break;
+        case "Update Employee's Manager":
+          updateEmployeeManager();
+          break;
+        case "Delete Employee":
+          deleteEmployee();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
 
 async function viewEmployees() {
   const query =
@@ -40,7 +114,7 @@ async function viewEmployees() {
     }
     console.log("\n");
     console.table(employeeArray);
-    serverJS.showMenu();
+    showMenu();
   });
 }
 
